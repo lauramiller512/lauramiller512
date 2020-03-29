fec503608026e9c36b32ccaf06a43bb6d35f0cbe
Laura Miller <lmiller.spence@gmail.com>
2020-03-29 15:39:18 +0100

    Creating a state to store values

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Search from './components/Search'
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=3fd375d9";
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
